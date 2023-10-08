2f551b14eb0dae57bbc35ae8ef1320a93cffbffd
ZhangZekaizkzkzk <zzksdxt@163.com>
2023-10-08 21:20:34 +0800

    修改了RDD实现原理的错误，flatMap的代码里补充了一个新例子，笔记中也做了对应补充。

--- src/main/Spark/_02trasnform/_04flatMap.scala
+++ src/main/Spark/_02trasnform/_04flatMap.scala
@@ -18,7 +18,7 @@ object _04flatMap {
     sc.stop()
   }
 
-  def main(args: Array[String]): Unit = {
+  def main2(args: Array[String]): Unit = {
     val sparkConf = new SparkConf().setMaster("local[*]").setAppName("Operator")
     val sc = new SparkContext(sparkConf)
 
@@ -34,4 +34,23 @@ object _04flatMap {
     rdd2.collect().foreach(println)
     sc.stop()
   }
+
+  def main(args: Array[String]): Unit = {
+    // 一个GPT给的例子
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("Operator")
+    val sc = new SparkContext(sparkConf)
+
+    // 创建一个包含多行文本的RDD
+    val data = Seq("Hello world", "Spark is amazing", "RDD flatMap example")
+    val rdd = sc.parallelize(data)
+
+    // 使用flatMap将每行文本分解成单词
+    val wordsRdd = rdd.flatMap(line => line.split(" "))
+
+    // 收集结果
+    val result = wordsRdd.collect().foreach(println)
+
+    sc.stop()
+  }
+
 }
